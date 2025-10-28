b3b0757c9a4ee3661eaa265c17287f9c0371c4fa
danzel <savostyanov19d@mail.ru>
2025-10-28 19:44:06 +0200

    update users table and delete email table

--- core/models/user.py
+++ core/models/user.py
@@ -1,34 +1,27 @@
 from typing import TYPE_CHECKING
 from typing import Optional, List
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 from core.models.base import Base
-
 
 if TYPE_CHECKING:
     from .post import Post
 
+
 class User(Base):
     __tablename__ = "users"
+    # __table_args__ = {'extend_existing': True}
 
     nickname: Mapped[str] = mapped_column(String(34), nullable=False, unique=True)
     name: Mapped[Optional[str]] = mapped_column(String(34))
     fullname: Mapped[Optional[str]]
-
-    addresses: Mapped['Email'] = relationship(back_populates="users",)
-
+    email: Mapped[str] = mapped_column(String,nullable=False, unique=True)
     posts: Mapped[list['Post']] = relationship(back_populates="users")
+
 
     def __repr__(self) -> str:
         return f'<User {self.id!r}, nickname: {self.nickname!r}, fullname: {self.fullname!r}, name: {self.name!r}>'
 
-class Email(Base):
-    __tablename__ = 'email_addresses'
 
-    email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
-
-    user: Mapped["User"] = relationship(back_populates='email_addresses')
-
-    def __repr__(self) -> str:
-        return f'Email(id={self.id!r}, email_address={self.email_address!r})'
+    def __str__(self) -> str:
+        return f'Nickname: {self.nickname!r},Username: {self.name!r}, fullname: {self.fullname!r}'
