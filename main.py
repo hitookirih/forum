a446d7765fb8142a45d5259491a0cf129b8b6c66
danzel <savostyanov19d@mail.ru>
2025-10-28 19:52:13 +0200

    no more create all on app start

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from contextlib import asynccontextmanager
 
-from core.models import Base, db_helper
 from fastapi import FastAPI
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 
