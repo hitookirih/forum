c5e84d5e9d8d219baffc503df8f68fa4dea63bcb
danzel <savostyanov19d@mail.ru>
2025-11-06 20:09:39 +0200

    adding password hashing via bcrypt

--- auth/utils.py
+++ auth/utils.py
@@ -1,4 +1,5 @@
 import jwt
+import bcrypt
 from core.config import settings
 
 
@@ -6,7 +7,7 @@ def encode_jwt(
         payload: dict,
         private_key: str = settings.auth_jwt.private_key_path.read_text(),
         algorithm: str = settings.auth_jwt.algorithm,
-):
+) -> str:
     encoded = jwt.encode(
         payload,
         private_key,
@@ -19,10 +20,30 @@ def decode_jwt(
         token: str | bytes,
         public_key: str = settings.auth_jwt.public_key_path.read_text(),
         algorithm: str = settings.auth_jwt.algorithm,
-):
+) -> dict:
     decoded = jwt.decode(
         token,
         public_key,
         algorithms=[algorithm],
     )
     return decoded
+
+
+def hash_password(
+    password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    # hashed = bcrypt.hashpw(pwd_bytes, salt)
+    # return hashed.decode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: str,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password.encode(),
+    )
