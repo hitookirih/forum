d7373bb6d73b4e43a4514629a1a66a9f6a0aecf7
danzel <savostyanov19d@mail.ru>
2026-01-13 19:46:09 +0200

    create router for jwt_auth

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,7 +1,9 @@
-
 from fastapi import APIRouter
 
 from .posts.views import router as posts_router
+from auth.views import router as auth_router
 
 router = APIRouter()
-router.include_router(router=posts_router, )
+router.include_router(router=posts_router, prefix="/posts")
+
+router.include_router(router=auth_router)
